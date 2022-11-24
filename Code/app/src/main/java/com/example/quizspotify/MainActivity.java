a9dbb1d1c141bfd0cb87771cf904816167d2f8d6
qRayne <kennafrayanerachid@gmail.com>
2022-11-24 13:25:15 -0500

    started jsonRequest

--- Code/app/src/main/java/com/example/quizspotify/MainActivity.java
+++ Code/app/src/main/java/com/example/quizspotify/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.quizspotify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    String url = "https://api.spotify.com/v1/artists/0eDvMgVFoNV3TpwtrVCoTj?si=c6jzRYGwSEWNWNyP3kXElA";
+    SharedPreferences sharedPreferences;
+    JsonObjectRequest jsonObjectRequest;
+    RequestQueueSingleton instance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // je crée la queue
+        // je créer un shared preferences et on get l'id de l'utilisateur
+        sharedPreferences  = getApplicationContext().getSharedPreferences("SPOTIFY", 0);
+
+        instance = RequestQueueSingleton.getInstance(this);
+
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println(response);
+            }
+        },null);
+
+        instance.addToRequestQueue(jsonObjectRequest);
+
+        @Override
+        public Map<String, String> getHeaders () throws AuthFailureError {
+            Map<String, String> headers = new HashMap<>();
+            String token = sharedPreferences.getString("token", "");
+            String auth = "Bearer " + token;
+            headers.put("Authorization", auth);
+            headers.put("Content-Type", "application/json; charset=utf-8");
+            return headers;
+        }
     }
-}
