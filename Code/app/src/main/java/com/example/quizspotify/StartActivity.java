7bb07441b43fa05f473cce86c3240b18600c73b1
qRayne <kennafrayanerachid@gmail.com>
2022-12-10 10:33:13 -0500

    finished animation

--- Code/app/src/main/java/com/example/quizspotify/StartActivity.java
+++ Code/app/src/main/java/com/example/quizspotify/StartActivity.java
@@ -1,44 +1,38 @@
 package com.example.quizspotify;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
 
-    Button question1,question2,question3;
-    Ecouteur ec;
+    Button start;
+    ImageView logoSpotify,logoQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        question1 = findViewById(R.id.buttonQuestion1);
-        question2 = findViewById(R.id.buttonQuestion2);
-        question3 = findViewById(R.id.buttonQuestion3);
-        ec = new Ecouteur();
+        // les find view by id
+        start = findViewById(R.id.buttonStart);
+        logoSpotify = findViewById(R.id.logoSpotify);
+        logoQuiz = findViewById(R.id.imageViewQuizLogo);
 
-        question1.setOnClickListener(ec);
-        question2.setOnClickListener(ec);
-        question3.setOnClickListener(ec);
+        // les animations
+        ObjectAnimator.ofFloat(logoSpotify, View.ROTATION, 0f, 360f).setDuration(5000).start();
+        ObjectAnimator.ofFloat(logoQuiz, View.TRANSLATION_X, -1000f, 0f).setDuration(5000).start();
 
-    }
 
-    private class Ecouteur implements View.OnClickListener{
-
-        @Override
-        public void onClick(View view) {
-            if (view.equals(question1))
-                startActivity(new Intent(StartActivity.this,Question1.class));
-            else if (view.equals(question2))
-                startActivity(new Intent(StartActivity.this,Question2.class));
-            else
-                startActivity(new Intent(StartActivity.this,Question3.class));
-        }
+        start.setOnClickListener(v -> {
+            Intent intent = new Intent(StartActivity.this, Question1.class);
+            startActivity(intent);
+        });
     }
 }
