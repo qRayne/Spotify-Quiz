e7a922e89f82cc0d4f08e040a337b0c3f3ae5b98
qRayne <kennafrayanerachid@gmail.com>
2022-12-10 11:34:02 -0500

    finished adding score to resultActivity

--- Code/app/src/main/java/com/example/quizspotify/ResultActivity.java
+++ Code/app/src/main/java/com/example/quizspotify/ResultActivity.java
@@ -2,17 +2,41 @@ package com.example.quizspotify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    ImageView imageViewScore;
+    TextView textViewFinalScore;
+    Button buttonRestart;
+    Bundle extras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        Bundle extras = getIntent().getExtras();
+        imageViewScore = findViewById(R.id.imageViewScore);
+        textViewFinalScore = findViewById(R.id.textViewFinalScore);
+        buttonRestart = findViewById(R.id.buttonRestart);
+        extras = getIntent().getExtras();
 
-        System.out.println(extras.getString("Score"));
+        // les animations
+        ObjectAnimator.ofFloat(imageViewScore, View.ROTATION, 0f, 360f).setDuration(5000).start();
+
+        // afficher le score
+        textViewFinalScore.setText(String.format("Votre score est de : %s", extras.get("Score")));
+
+        buttonRestart.setOnClickListener(v -> {
+            Intent intent = new Intent(ResultActivity.this, Question1.class);
+            startActivity(intent);
+        });
+
     }
 }
